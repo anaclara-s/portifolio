872736239cca5c0a856ac49c49a920187b9d7c1d
Ana <anaclarasantos.amorin@gmail.com>
2025-04-30 15:21:00 -0300

    adcionado a pagina Home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { FaCode, FaLinkedin, FaGithub } from "react-icons/fa";
 
 import './Header.css';
 
@@ -6,6 +7,11 @@ const Header = () => {
     return (
         <div className='header-wrapped'>
             <header className="header">
+                <div className="left-section">
+                    <FaCode className="icon" />
+                    <span className="brand">Ana.dev</span>
+                </div>
+
                 <nav className='menu'>
                     <Link to="/">Início</Link>
                     <Link to="/sobre">Sobre mim</Link>
@@ -13,6 +19,15 @@ const Header = () => {
                     <Link to="/projetos">Projetos</Link>
                     <Link to="/contato">Contato</Link>
                 </nav>
+
+                <div className="right-section">
+                    <a href="https://www.linkedin.com/in/anaclaradev/" target="_blank" rel="noopener nereferrer">
+                        <FaLinkedin className="icon" />
+                    </a>
+                    <a href="https://github.com/anaclara-s" target="_blank" rel="noopener noreferrer">
+                        <FaGithub className="icon" />
+                    </a>
+                </div>
             </header>
         </div>
     );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,9 @@ import './Home.css';
 
 const Home = () => {
     return (
-        <div className='body'>
+        <div className='home-container'>
+            <p className='intro-text'>Olá, meu nome é Ana Clara sou</p>
+            <h1 className='typing-text'>Desenvolvedora Mobile e Web</h1>
         </div>
     );
 };
