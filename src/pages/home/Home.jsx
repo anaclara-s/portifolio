1b00dff09e339c3ce1bab7e244dbd4fefa78de50
Ana <anaclarasantos.amorin@gmail.com>
2025-05-13 14:00:25 -0300

    adcionado responsividade em home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div className='home-container'>
             <p className='intro-text'>Olá, meu nome é Ana Clara sou</p>
-            <h1 className='typing-text'>Desenvolvedora Mobile e Web</h1>
+            <h1 className='typing-text'>Desenvolvedora Mobile</h1>
         </div>
     );
 };
