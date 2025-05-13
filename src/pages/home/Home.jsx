268491714150d5963fb12e2d5c8304a23c87844e
Ana <anaclarasantos.amorin@gmail.com>
2025-05-13 15:20:25 -0300

    imagem, animação e responsividade Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,23 @@
 import './Home.css';
+import illustration from '../../assets/home/illustration.png'
 
 const Home = () => {
     return (
         <div className='home-container'>
-            <p className='intro-text'>Olá, meu nome é Ana Clara sou</p>
-            <h1 className='typing-text'>Desenvolvedora Mobile</h1>
+            <div className='home-wrapper'>
+
+                <div className='text-section'>
+                    <p className='intro-text'>Olá, meu nome é Ana Clara sou</p>
+                    <h1 className='typing-text'>Desenvolvedora Mobile</h1>
+                </div>
+
+                <div className='image-section'>
+                    <div className='image-wrapper'>
+                        <img src={illustration} alt="Ilustração" />
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
