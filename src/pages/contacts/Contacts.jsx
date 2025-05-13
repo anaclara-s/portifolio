d67c54f6ec88ed114682b5c6d79664e4c5c01d6b
Ana <anaclarasantos.amorin@gmail.com>
2025-05-13 12:43:43 -0300

    adcionado responsividade e informações em contacts

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -11,24 +11,27 @@ const Contact = () => {
             <div className="contact-content">
                 <h2 className='title'>Entre em contato</h2>
                 <p className='subtitle'>Fique a vontade para entrar em contato caso tenha alguma duvida ou oportunidade</p>
-                
+
                 <div className="contact-icons">
                     <div className="icon-item">
-                        <a href="mailto:vazio por enquanto" className="icon-circle">
+                        <a href="mailto:acsantos.amorin@gmail.com" className="icon-circle">
                             <MdOutlineEmail className="icon" />
                         </a>
                         <p className="icon-label">Email</p>
-                        <p className="icon-contact">vazio por enquanto</p>
+                        <p className="icon-contact">acsantos.amorin@gmail.com</p>
                     </div>
-                    
+
                     <div className="icon-item">
                         <div className="icon-circle">
-                            <FaWhatsapp className="icon" />
+                            <a href="https://wa.me/5531996553517" target="_blank" rel="noopener noreferrer" className="icon-circle">
+                                <FaWhatsapp className="icon" />
+                            </a>
+
                         </div>
                         <p className="icon-label">Telefone</p>
-                        <p className="icon-contact">vazio por enquanto</p>
+                        <p className="icon-contact">(31) 9 9655-3517</p>
                     </div>
-                    
+
                     <div className="icon-item">
                         <a href="https://www.linkedin.com/in/anaclaradev/" target="_blank" rel="noopener noreferrer" className="icon-circle">
                             <FaLinkedinIn className="icon" />
@@ -36,7 +39,7 @@ const Contact = () => {
                         <p className="icon-label">LinkedIn</p>
                         <p className="icon-contact">@anaclaradev</p>
                     </div>
-                    
+
                     <div className="icon-item">
                         <a href="https://github.com/anaclara-s" target="_blank" rel="noopener noreferrer" className="icon-circle">
                             <FiGithub className="icon" />
